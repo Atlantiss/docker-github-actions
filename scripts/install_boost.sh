94ef0f659dba527e43bd98d4c7a126b871aed412
Bartosz Korczyński <b.andysc@gmail.com>
2021-12-24 01:27:30 +0000

    Include boost json lib

--- scripts/install_boost.sh
+++ scripts/install_boost.sh
@@ -12,7 +12,7 @@ curl -SL https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/sou
 cd boost_${BOOST_FILE_NAME}
 
 ./bootstrap.sh \
-    --with-libraries=system,filesystem,program_options,iostreams,regex,container 
+    --with-libraries=system,filesystem,program_options,iostreams,regex,container,json 
 ./b2 toolset=gcc cxxstd=11 --build-dir=/tmp/build-boost \
 ./b2 install -j 8
 
